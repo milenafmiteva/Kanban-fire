be1b2c5b6612d8ac400c4c3d5a2f086e0dfa16e4
milenafmiteva <milenafmiteva@abv.bg>
2021-02-05 17:34:16 +0200

    Commit 2

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,4 @@
-import { Input } from '@angular/core';
-import { Component, OnInit, Output } from '@angular/core';
-import * as EventEmitter from 'events';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task';
 
 @Component({
@@ -8,14 +6,11 @@ import { Task } from './task';
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent {
 
   @Input() task: Task;
-  @Output() edit = new EventEmitter();
+  @Output() edit = new EventEmitter<Task>();
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -2,4 +2,4 @@ export interface Task {
     id?: string;
     title: string;
     description: string;
-}
+  }
